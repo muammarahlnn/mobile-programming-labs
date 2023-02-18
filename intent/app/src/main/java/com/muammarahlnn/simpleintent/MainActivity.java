24d7e680ae0e3b5a78e52e89c9626d2a35c44798
muammarahlnn <21muammarahlan@gmail.com>
2023-02-18 16:13:20 +0800

    add implicit intent

--- intent/app/src/main/java/com/muammarahlnn/simpleintent/MainActivity.java
+++ intent/app/src/main/java/com/muammarahlnn/simpleintent/MainActivity.java
@@ -3,7 +3,12 @@ package com.muammarahlnn.simpleintent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -15,6 +20,15 @@ public class MainActivity extends AppCompatActivity {
     public static final String TYPE_DATA = "data";
     public static final String TYPE_OBJECT = "object";
 
+    private final ActivityResultLauncher<Intent> launcherPickPhoto = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(), result -> {
+            String text = result.getResultCode() == RESULT_OK
+                    ? "Success picking a photo"
+                    : "Not picking a photo";
+            Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+        }
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnMoveData = findViewById(R.id.btn_move_data);
         Button btnMoveObject = findViewById(R.id.btn_move_object);
+        Button btnPickPhoto = findViewById(R.id.btn_pick_photo);
 
         String name = "Muammar Ahlan Abimanyu";
         String nim = "H071191032";
@@ -39,6 +54,12 @@ public class MainActivity extends AppCompatActivity {
             toDetail.putExtra(DetailActivity.EXTRA_TYPE, TYPE_OBJECT);
             toDetail.putExtra(DetailActivity.EXTRA_STUDENT, student);
             startActivity(toDetail);
+        });
+
+        btnPickPhoto.setOnClickListener(view -> {
+            Intent intentPicker = new Intent(Intent.ACTION_GET_CONTENT);
+            intentPicker.setType("image/*");
+            launcherPickPhoto.launch(Intent.createChooser(intentPicker, "Pick a photo"));
         });
     }
 }
