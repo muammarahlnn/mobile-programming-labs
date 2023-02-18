476c9781474857c474ba1dd623e78056223d0d27
muammarahlnn <21muammarahlan@gmail.com>
2023-02-18 16:14:17 +0800

    remove unused imports

--- intent/app/src/main/java/com/muammarahlnn/simpleintent/MainActivity.java
+++ intent/app/src/main/java/com/muammarahlnn/simpleintent/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
